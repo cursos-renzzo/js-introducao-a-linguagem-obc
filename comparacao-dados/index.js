f1fc1fd00d4d224ec46318d11637afed098a6eb9
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2024-05-28 15:17:22 -0300

    feat: adding parallel data page

---
+++ comparacao-dados/index.js
@@ -0,0 +1,11 @@
+console.log(3 != 3)
+console.log(5 != 3)
+console.log(5 != "5")
+console.log(5 != 3)
+console.log(5 !== "5")
+console.log(4 > 3)
+console.log(5 > 10)
+console.log(5 >= 5)
+console.log(6 <= 3)
+console.log(6 < 6)
+
