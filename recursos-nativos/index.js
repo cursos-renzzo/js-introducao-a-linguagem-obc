9ee7c28376a8c7e370255574146a3931877c1c33
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2024-05-28 15:15:54 -0300

    feat: add html js native resources page

---
+++ recursos-nativos/index.js
@@ -0,0 +1,15 @@
+// let nome = "Renzzo";
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+// window.alert(nome)
+// window.prompt(nome);
+
+
+nome = window.prompt("Insira o seu nome: ");
+let confirmacao = window.confirm("Confirma essa ação?");
+
+alert("Olá mundo!")
+
