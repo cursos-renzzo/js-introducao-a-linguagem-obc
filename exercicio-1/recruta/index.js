88293c4c0c3a948704c32121b495a1d0cabfab61
Renzzo Rocha <127625871+renzzorocha@users.noreply.github.com>
2024-05-28 15:23:50 -0300

    feat: add some exercises
    i forgot to add this one...

---
+++ exercicio-1/recruta/index.js
@@ -0,0 +1,11 @@
+const primeiroNomeDoRecruta = prompt("Informe, por gentileza, o seu primeiro nome: ");
+const sobrenomeDoRecruta = prompt("Informe, por gentileza, o seu sobrenome: ");
+const campoDeEstudoRecruta = prompt("Informe, por gentileza, o seu campo de estudo: ");
+const anoDeNascimentoRecruta = prompt("Informe, por gentileza, o seu ano de nascimento: ");
+
+alert (
+"Recruta cadastrado com sucesso!" + "\n" +
+"Nome completo: " + primeiroNomeDoRecruta + " " + sobrenomeDoRecruta + "\n" +
+"Campo de estudo: " + campoDeEstudoRecruta + "\n" + 
+"Ano de nascimento: " + anoDeNascimentoRecruta 
+)
